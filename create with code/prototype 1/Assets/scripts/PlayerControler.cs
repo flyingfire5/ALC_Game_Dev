c31991cb7365bbc1881207aaa7badf1fc49e99cc
junior <junior.c854@stu.nebo.edu>
2019-09-13 10:29:34 -0600

    turning

--- create with code/prototype 1/Assets/scripts/PlayerControler.cs
+++ create with code/prototype 1/Assets/scripts/PlayerControler.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerControler : MonoBehaviour
 {
     public float speed;
+    public float turnspeed;
+    public float horiImput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +16,10 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        horiImput = Input.GetAxis("Horizontal");
         //move the car forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed );
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnspeed * horiImput);
     }
 }
