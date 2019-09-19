46bdc9e7e9c15f5644d39bbd0c5977d19ce84694
junior <junior.c854@stu.nebo.edu>
2019-09-19 10:28:51 -0600

    oof

---
+++ create with code/prototype 1/Assets/Challenge 1/Scripts/proX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class proX : MonoBehaviour
+{
+    public float spin;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // makes propeler spin
+        transform.Rotate(Vector3.forward * spin * Time.deltaTime);
+    }
+}
