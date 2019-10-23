95213c550f62a534469fb1a835fbd8121699844b
junior <junior.c854@stu.nebo.edu>
2019-10-23 10:30:32 -0600

    chalenge

--- create with code/prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ create with code/prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -12,21 +12,41 @@ public class SpawnManagerX : MonoBehaviour
 
     private float startDelay = 1.0f;
     private float spawnInterval = 4.0f;
+    private float nextBall;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        //InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        //Invoke("SpawnRandomBall", Random.Range(3,5));
+    }
+
+    void Update()
+    {
+        if (Time.time > nextBall)
+        {
+            nextBall += Random.Range(3.0f, 5.0f);
+            // Generate random ball index and random spawn position
+            //spawnInterval = Random.Range(3, 5);
+            int ballIndex = Random.Range(0, ballPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+            // instantiate ball at random spawn location
+            Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+        }
     }
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
         // Generate random ball index and random spawn position
+        //spawnInterval = Random.Range(3, 5);
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+
     }
 
 }
