3917b4e71b746b626798f91eb7510d060e55b608
junior <junior.c854@stu.nebo.edu>
2019-11-12 10:28:17 -0700

    start

---
+++ create with code/prototype 3/prototype 3/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed = 30;
+    private PlayerControler playerControllerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerControllerScript = GameObject.Find('Player').GetComponent<PlayerCotroler>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+    }
+}
---
+++ create with code/prototype 3/prototype 3/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifier;
+    public bool isOnGround;
+    public bool gameOver;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision soyouthinkyoucandance)
+    {
+        if (soyouthinkyoucandance.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        } else if (soyouthinkyoucandance.gameObject.CompareTag("obstical"))
+        {
+            gameOver = true;
+        }
+    }
+}
