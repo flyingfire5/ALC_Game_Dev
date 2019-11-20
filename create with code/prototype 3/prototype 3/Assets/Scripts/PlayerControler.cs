141de8e55c0ae439fac05907d4337620562da2d1
junior <junior.c854@stu.nebo.edu>
2019-11-20 10:29:04 -0700

    unity

--- create with code/prototype 3/prototype 3/Assets/Scripts/PlayerControler.cs
+++ create with code/prototype 3/prototype 3/Assets/Scripts/PlayerControler.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerControler : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround;
@@ -13,16 +16,19 @@ public class PlayerControler : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            //playerAnim.SetTrigger(“Jump_Trig”);
+            dirtParticle.Stop();
         }
     }
 
@@ -31,10 +37,13 @@ public class PlayerControler : MonoBehaviour
         if (soyouthinkyoucandance.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         } else if (soyouthinkyoucandance.gameObject.CompareTag("Obstical"))
         {
             Debug.Log("game over");
             gameOver = true;
+            explosionParticle.Play();
+            dirtParticle.Stop();
         }
     }
 }
