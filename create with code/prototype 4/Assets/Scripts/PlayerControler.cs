aae08db493ab0770df29d881637a933961912858
junior <junior.c854@stu.nebo.edu>
2019-12-11 10:30:05 -0700

    epic

---
+++ create with code/prototype 4/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody playerRB;
+    private GameObject focalpoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        focalpoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRB.AddForce(focalpoint.transform.forward * speed * forwardInput);
+    }
+}
