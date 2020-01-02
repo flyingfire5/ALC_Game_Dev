347861dd667832c657e3449689abf2be749293fe
junior <junior.c854@stu.nebo.edu>
2020-01-02 10:25:28 -0700

    epic

--- create with code/prototype 4/Assets/Scripts/SpawnManager.cs
+++ create with code/prototype 4/Assets/Scripts/SpawnManager.cs
@@ -6,19 +6,29 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
     private float spawnRange = 9;
+    public int enemyCount;
+    public int waveNumber = 1;
+    public GameObject powerup;
 
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(enemyPrefab, GenorateSpawnPosition(), enemyPrefab.transform.rotation);
-        spawnEnemyWave(3);
+        Instantiate(powerup, GenorateSpawnPosition(), powerup.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            Instantiate(powerup, GenorateSpawnPosition(), powerup.transform.rotation);
+            waveNumber++;
+            spawnEnemyWave(waveNumber);
+        }
+    } 
+
 
     private Vector3 GenorateSpawnPosition()
     {
